3e82f6e08114782e7aefb7196fd8ab766656775e
Yash Singh <myemail>
2025-02-02 13:16:59 +0530

    full functional

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-secret-key")
 
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ['*']
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
